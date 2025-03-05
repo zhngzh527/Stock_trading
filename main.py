9c717c9df1cf8b8124a35bb3e8ae39300595a8b3
zhongcheng zhao <zhongcheng9114@gmail.com>
2025-03-05 12:08:40 -0600

    NEWS API

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import requests
 import os
+import numpy as np
 
 api_key = os.getenv("API_KEY")
 parameters = {
     "function" : "TIME_SERIES_DAILY",
     "symbol" : "NVDA",
     "apikey" : api_key,
+}
+
+news_key = os.getenv("NEWS_KEY")
+news_parameters = {
+    "apiKey": "d40106b34aa54202886ec23c615ee9f1",
+    "q": "Nvidia",
+    "from": "2025-03-04",
+    "to": "2025-03-04",
+    "pageSize":10,
+    "sortBy":"popularity"
 }
 
 STOCK_NAME = "NVDA"
@@ -30,23 +41,25 @@ the_day_before_yesterday_closing_price = the_day_before_yesterday_data["4. close
 print(the_day_before_yesterday_closing_price)
 
 #------------------------GET THE % DIFFERENCE -------------------#
-def percentage(yesterday_closing_price,the_day_before_yesterday_closing_price):
-    price_changed = 100 *(abs(float(yesterday_closing_price) - float(the_day_before_yesterday_closing_price))
-                     / float(yesterday_closing_price))
-    return str(price_changed) + "%"
+def percentage(x, y):
+    price_changed = 100 *(abs(float(x) - float(y))
+                          / float(x))
+    round_num = np.around(price_changed, 2)
+    return str(round_num) + "%"
 
-difference = percentage(114.0600,124.92)
+difference = percentage(yesterday_closing_price,the_day_before_yesterday_closing_price)
 print(difference)
 
 
 
-    ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
-# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+news_response = requests.get(url=NEWS_ENDPOINT,params=news_parameters)
+news_response.raise_for_status()
+news = news_response.json()['articles']
 
+# new_list = [expression for item in iterable if condition]
 
-#TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
-
-#TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
+news_list = [news['title'] for news in news]
+print(news_list)
 
 #TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
 
@@ -58,12 +71,8 @@ print(difference)
 #TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
 
 
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number. 
-
 #TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
 
-#TODO 9. - Send each article as a separate message via Twilio. 
 
 
 
